362faa61e0e30af0741effb1df99280f0b929b11
Tongshen <lsmomy@gmail.com>
2016-11-06 17:20:07 +0100

    CyclicRotation
    https://codility.com/programmers/lessons/2-arrays/cyclic_rotation/

--- src/Solution.java
+++ src/Solution.java
@@ -34,12 +34,24 @@ class Solution {
     		}
 		return A[N-1];
     }
+    	int N = A.length;
+    	if(N == 0)
+    		return A;
+    	if(K>=N)
+    		K = K%N;
+    	K = N-K;
+    	int[] solution = new int[N];
+    	for(int i= 0; i<N ; i++){
+    		int position = i + K;
+    		if(position < N)
+    			solution[i] = A[position];
+    		else 
+    			solution[i] = A[position-N];
+    	}
+    	return solution;
+    }
     
     public static void main(String[] args){
     	Solution T = new Solution();
-    	//T.binary_gap(15);
-    	int[] A = {0,0,2,2,4};
-    	int n = T.OddOccurrencesInArray(A, 5);
-    	System.out.println(n);
     }
 }
